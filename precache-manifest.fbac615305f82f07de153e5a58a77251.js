e4311a3220ea1af08b378ed4d8a287ba288e829c
Aravindh-SNR <aravindhsnr@gmail.com>
2019-10-23 23:23:50 +0530

    Updates

---
+++ precache-manifest.fbac615305f82f07de153e5a58a77251.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "484283e413a5d48d9d324a70d616b06d",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "91679f059dadda280dcf",
+    "url": "/robofriends/static/css/main.8c2ba8d4.chunk.css"
+  },
+  {
+    "revision": "a21fd42ca8386be1229c",
+    "url": "/robofriends/static/js/2.fe0eadad.chunk.js"
+  },
+  {
+    "revision": "91679f059dadda280dcf",
+    "url": "/robofriends/static/js/main.1e73ed4e.chunk.js"
+  },
+  {
+    "revision": "69ae533dd49bc2dbc7cd",
+    "url": "/robofriends/static/js/runtime-main.9f59819d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
